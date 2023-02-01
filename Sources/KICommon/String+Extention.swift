57f5e2591fdfb72ee6f6f6e98e3dac2e2a1b606c
Lukas-Song <lukas@kis-global.com>
2023-02-01 09:49:09 +0900

    String Extension 추가

---
+++ Sources/KICommon/String+Extention.swift
@@ -0,0 +1,15 @@
+//
+//  String+Extention.swift
+//
+//
+//  Created by Lukas on 2023/02/01.
+//
+
+import Foundation
+
+public extension String {
+    
+    var won: String {
+        return self + "Won"
+    }
+}
